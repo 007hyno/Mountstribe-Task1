a69512f3f4d2a5220923b0048ef77964be00729f
007hyno <Parasrawat05@gmail.com>
2023-02-13 14:07:17 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+import reportWebVitals from './reportWebVitals';
 
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+
+  <React.StrictMode>
+  <App />
+</React.StrictMode>
+
+  // <BrowserRouter>
+  //   <App />
+  // </BrowserRouter>
 );
+reportWebVitals();
